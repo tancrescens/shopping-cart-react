ad550e55a97f2ca1187c89d680862fe33f326c6b
Crescens <2e3crescens@gmail.com>
2024-01-24 17:30:11 +0800

    fixed syntax error of imports

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import "'bootstrap/dist/css/bootstrap.css";
-import "'bootstrap/dist/css/bootstrap.js";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/css/bootstrap.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
